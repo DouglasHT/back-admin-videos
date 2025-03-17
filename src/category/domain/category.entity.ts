1918cee2367e3efd11c8ff81f19f1eae187c1120
Douglas Tripoloni <douglas.htri@gmail.com>
2025-03-17 15:22:07 -0300

    Revert "feature: category fake builder"
    This reverts commit b4dc936506fd235ffa1590be184458aaa4092231.

--- src/category/domain/category.entity.ts
+++ src/category/domain/category.entity.ts
@@ -2,7 +2,6 @@ import { Entity } from "../../shared/domain/entity";
 import { EntityValidationError } from "../../shared/domain/validators/validation.error";
 import { ValueObject } from "../../shared/domain/value-object";
 import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
-import { CategoryFakeBuilder } from "./category-fake.builder";
 import { CategoryValidatorFactory } from "./category.validator";
 
 export type CategoryConstructorProps = {
@@ -70,10 +69,6 @@ export class Category extends Entity {
     if (!isValid) {
       throw new EntityValidationError(validator.errors);
     }
-  }
-
-  static fake() {
-    return CategoryFakeBuilder;
   }
 
   toJSON() {
