744a0beb7cc8efb69bd6694d09c79521d1ae01a8
Douglas <douglas.htri@gmail.com>
2025-04-01 20:49:41 -0300

    feature: test find by id, update, and throw not found

--- src/category/infra/db/sequelize/__tests__/category-sequelize.repository.int-spec.ts
+++ src/category/infra/db/sequelize/__tests__/category-sequelize.repository.int-spec.ts
@@ -2,6 +2,8 @@ import { Sequelize } from "sequelize-typescript";
 import { CategoryModel } from "../category.model";
 import { CategorySequelizeRepository } from "../category-sequelize.repository";
 import { Category } from "../../../../domain/category.entity";
+import { Uuid } from "../../../../../shared/domain/value-objects/uuid.vo";
+import { NotFoundError } from "../../../../../shared/domain/errors/not-found.error";
 
 describe("CategorySequelizeRepository Integration Test", () => {
   let sequelize;
@@ -17,10 +19,39 @@ describe("CategorySequelizeRepository Integration Test", () => {
     repository = new CategorySequelizeRepository(CategoryModel);
   });
 
-  test("should insert a category", async () => {
+  it("should insert a new category", async () => {
     let category = Category.fake().aCategory().build();
     await repository.insert(category);
     let entity = await repository.findById(category.category_id);
     expect(entity.toJSON()).toStrictEqual(category.toJSON());
+  });
+
+  it("should finds a entity by id", async () => {
+    let entityFound = await repository.findById(new Uuid());
+    expect(entityFound).toBeNull();
+
+    const entity = Category.fake().aCategory().build();
+    await repository.insert(entity);
+    entityFound = await repository.findById(entity.category_id);
+    expect(entityFound.toJSON()).toStrictEqual(entity.toJSON());
+  });
+
+  it("should throw error on update when a entity not found", async () => {
+    const entity = Category.fake().aCategory().build();
+
+    await expect(repository.update(entity)).rejects.toThrow(
+      new NotFoundError(entity.category_id.id, Category)
+    );
+  });
+
+  it("should update entity", async () => {
+    const entity = Category.fake().aCategory().build();
+    await repository.insert(entity);
+
+    entity.changeName("Movie updated");
+    await repository.update(entity);
+
+    const entityFound = await repository.findById(entity.category_id);
+    expect(entityFound.toJSON()).toStrictEqual(entity.toJSON());
   });
 });
